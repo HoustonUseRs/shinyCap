f659c0d345b0d85631dfb74d98ac2d86ad9f7f24
Sahil Seth <sahiilseth@gmail.com>
2016-05-16 20:55:42 -0500

    updating docs, adding dependencies of other packages.

--- R/shinyCap.R
+++ R/shinyCap.R
@@ -1,23 +1,23 @@
 #' exampleMod
 #'
-#' @param input 
-#' @param output 
-#' @param session 
+#' @param input
+#' @param output
+#' @param session
 #' @param conn_fun
 #' @export
 exampleMod <- function(input, output, session,conn_fun) {
   output$distPlot <- renderPlot({
     hist(rnorm(input$obs), col = 'darkgray', border = 'white')
   })
-  
+
   react_return_fun <- eventReactive(input$submit, {
-    df <- data.frame(rnorm = input$obs, 
-                     text_col = input$text_inp, 
+    df <- data.frame(rnorm = input$obs,
+                     text_col = input$text_inp,
                      date_col = input$date_inp,
                      time = Sys.time())
     return(df)
   })
-  
+
   observeEvent(input$submit, {
     dbWriteTable(conn_fun(), "TABLE_NAME",
                  react_return_fun(), append = TRUE,
@@ -44,117 +44,118 @@ exampleModUI <- function(id) {
 }
 
 
-#' shinyCap 
+#' shinyCap
+#' @import shiny dplyr RPostgreSQL sqldf
 #' @export
 shinyCap <- function() {
 
-require(shiny)
-require(dplyr)
-require(RPostgreSQL)
-require(sqldf)
-shinyApp(
-  ui =
-    navbarPage("shinyCap",
-               tabPanel("Database Level",
-                        navlistPanel("Table CRUD Operations",
-                          tabPanel("Define Database Connection",
-                            textInput("user_id",
-                                      "Enter a Valid Username",
-                                      value = "postgres"),
-                            passwordInput("password_id",
-                                          "Enter posgres user password"),
-                            p("Password will display for testing"),
-                            textInput("dbname_id",
-                                      "Enter/Update Database Name",
-                                      value = "postgres"),
-                            textInput("ip_id",
-                                      "Enter Server IP address",
-                                      value = "localhost"),
-                            numericInput("port_id", 
-                                         "Enter Port #",
-                                         value = 5432),
-                            actionButton("conn_button", "Connect"),
-                            textOutput("passwd_print")
-                          ),
-                       tabPanel("Create New Database",
-                                  textInput("create_db",
-                                            "Define database Name"
-                                  ),
-                                  numericInput("conn_limit",
-                                               "Max Connection Limit",
-                                               value = 10),
-                                  actionButton("db_submit",
-                                               "Create Database")
-                       )
-                        )),
-               tabPanel("Table Level",
-                       navlistPanel("Table CRUD Operations",
-                         tabPanel("Create New Table",
-                          textInput("create_tb",
-                                    "Create Table"),
-                          textInput("pk_id",
-                                    "Name of Primary Key Column"),
-                          p("Primary Key defaults to auto-increment serial datatype, uses user input as table owner"),
-                          actionButton("create_tb_button", "Create Table")
-                         ),
-                         tabPanel("Create Table from File",
-                                  fileInput('upload_tbl', 'Choose CSV File',
-                                            accept=c('text/csv', 
-                                                     'text/comma-separated-values,text/plain', 
-                                                     '.csv')),
-                                  dataTableOutput("read_file_view")
-                                  ),
-                         tabPanel("Select Table",
-                                  p("Requires Valid Database Connection to be submitted"),
-                                  uiOutput("tbl_select")
-                         ),
-                         tabPanel("Table Fields",
-                                  p("Requires Valid Table to be Selected"),
-                          textInput("new_col_id", "New Column Name"),
-                          selectizeInput("col_type", "Column Data Type",
-                                         choices  = c("text", "double precision", "boolean", "date")),
-                          actionButton("new_col_button", "New Column/Field")
-                         )
-                        )
-               ),
-               tabPanel("Row Level",
-                        fluidPage(
-                          exampleModUI("my_mod"),
-                          verbatimTextOutput("my_mean")
-                        )
-               )
-    ),
-server = function(input, output) {
-  
-  ### Example insert statement with proper syntax
-  # query_tmp <- 
-  #"INSERT INTO data_cap (mpg, cyl, disp, hp, drat, wt, qsec, vs, am, gear, carb) VALUES (21, 22, 3.9, 2.62, 16.46, 0, 1, 4, 4, 3, 3), (21, 22, 3.9, 2.62, 16.46, 0, 1, 4, 4, 3, 3)"
-  
-  drv <- PostgreSQL()
-  # update_db_conn
-  observeEvent(input$conn_button, {
-  options(sqldf.RPostgreSQL.user = input$user_id, 
-          sqldf.RPostgreSQL.password = input$password_id,
-          sqldf.RPostgreSQL.dbname =input$dbname_id,
-          sqldf.RPostgreSQL.host =input$ip_id, 
-          sqldf.RPostgreSQL.port =input$port_id)
-  })
-  
-  output$passwd_print <-
-    renderText(input$password)
-  
-  observeEvent(input$db_submit, {
-  sqldf(sprintf("CREATE DATABASE %s
+  #require(shiny)
+  #require(dplyr)
+  #require(RPostgreSQL)
+  #require(sqldf)
+  shinyApp(
+    ui =
+      navbarPage("shinyCap",
+                 tabPanel("Database Level",
+                          navlistPanel("Table CRUD Operations",
+                                       tabPanel("Define Database Connection",
+                                                textInput("user_id",
+                                                          "Enter a Valid Username",
+                                                          value = "postgres"),
+                                                passwordInput("password_id",
+                                                              "Enter posgres user password"),
+                                                p("Password will display for testing"),
+                                                textInput("dbname_id",
+                                                          "Enter/Update Database Name",
+                                                          value = "postgres"),
+                                                textInput("ip_id",
+                                                          "Enter Server IP address",
+                                                          value = "localhost"),
+                                                numericInput("port_id",
+                                                             "Enter Port #",
+                                                             value = 5432),
+                                                actionButton("conn_button", "Connect"),
+                                                textOutput("passwd_print")
+                                       ),
+                                       tabPanel("Create New Database",
+                                                textInput("create_db",
+                                                          "Define database Name"
+                                                ),
+                                                numericInput("conn_limit",
+                                                             "Max Connection Limit",
+                                                             value = 10),
+                                                actionButton("db_submit",
+                                                             "Create Database")
+                                       )
+                          )),
+                 tabPanel("Table Level",
+                          navlistPanel("Table CRUD Operations",
+                                       tabPanel("Create New Table",
+                                                textInput("create_tb",
+                                                          "Create Table"),
+                                                textInput("pk_id",
+                                                          "Name of Primary Key Column"),
+                                                p("Primary Key defaults to auto-increment serial datatype, uses user input as table owner"),
+                                                actionButton("create_tb_button", "Create Table")
+                                       ),
+                                       tabPanel("Create Table from File",
+                                                fileInput('upload_tbl', 'Choose CSV File',
+                                                          accept=c('text/csv',
+                                                                   'text/comma-separated-values,text/plain',
+                                                                   '.csv')),
+                                                dataTableOutput("read_file_view")
+                                       ),
+                                       tabPanel("Select Table",
+                                                p("Requires Valid Database Connection to be submitted"),
+                                                uiOutput("tbl_select")
+                                       ),
+                                       tabPanel("Table Fields",
+                                                p("Requires Valid Table to be Selected"),
+                                                textInput("new_col_id", "New Column Name"),
+                                                selectizeInput("col_type", "Column Data Type",
+                                                               choices  = c("text", "double precision", "boolean", "date")),
+                                                actionButton("new_col_button", "New Column/Field")
+                                       )
+                          )
+                 ),
+                 tabPanel("Row Level",
+                          fluidPage(
+                            exampleModUI("my_mod"),
+                            verbatimTextOutput("my_mean")
+                          )
+                 )
+      ),
+    server = function(input, output) {
+
+      ### Example insert statement with proper syntax
+      # query_tmp <-
+      #"INSERT INTO data_cap (mpg, cyl, disp, hp, drat, wt, qsec, vs, am, gear, carb) VALUES (21, 22, 3.9, 2.62, 16.46, 0, 1, 4, 4, 3, 3), (21, 22, 3.9, 2.62, 16.46, 0, 1, 4, 4, 3, 3)"
+
+      drv <- PostgreSQL()
+      # update_db_conn
+      observeEvent(input$conn_button, {
+        options(sqldf.RPostgreSQL.user = input$user_id,
+                sqldf.RPostgreSQL.password = input$password_id,
+                sqldf.RPostgreSQL.dbname =input$dbname_id,
+                sqldf.RPostgreSQL.host =input$ip_id,
+                sqldf.RPostgreSQL.port =input$port_id)
+      })
+
+      output$passwd_print <-
+        renderText(input$password)
+
+      observeEvent(input$db_submit, {
+        sqldf(sprintf("CREATE DATABASE %s
         WITH OWNER = postgres
         ENCODING = 'UTF8'
         TABLESPACE = pg_default
         CONNECTION LIMIT = %s;", input$create_db, input$conn_limit))
-  })
+      })
 
-  observeEvent(input$create_tb_button, {
-    sqldf(
-      sprintf(
-      "CREATE TABLE %s
+      observeEvent(input$create_tb_button, {
+        sqldf(
+          sprintf(
+            "CREATE TABLE %s
       (
       %s serial NOT NULL,
       CONSTRAINT tbl_pk PRIMARY KEY (%s)
@@ -163,78 +164,78 @@ server = function(input, output) {
       OIDS=FALSE
       );
       ALTER TABLE %s
-      OWNER TO %s;", input$create_tb, 
-      input$pk_id, input$pk_id, 
-      input$create_tb, input$user_id)
-    )
-  })
-  
-conn_fun <- reactive({
-  drv <- PostgreSQL()
-  conn <- dbConnect(drv,
-            dbname  = input$dbname_id,
-            host = input$ip_id,
-            port = input$port_id,
-            user = input$user_id,
-            password = input$password_id)
-  return(conn)
-  })
+      OWNER TO %s;", input$create_tb,
+            input$pk_id, input$pk_id,
+            input$create_tb, input$user_id)
+        )
+      })
 
-dplyr_conn <- eventReactive(input$conn_button, {
-  src_postgres(dbname = input$dbname_id,
-               host = input$ip_id,
-               port = input$port_id,
-               user = input$user_id,
-               password = input$password_id)
-})
+      conn_fun <- reactive({
+        drv <- PostgreSQL()
+        conn <- dbConnect(drv,
+                          dbname  = input$dbname_id,
+                          host = input$ip_id,
+                          port = input$port_id,
+                          user = input$user_id,
+                          password = input$password_id)
+        return(conn)
+      })
 
-  observeEvent(input$new_col_button, {
-    dbSendQuery(conn_fun(),sprintf(
+      dplyr_conn <- eventReactive(input$conn_button, {
+        src_postgres(dbname = input$dbname_id,
+                     host = input$ip_id,
+                     port = input$port_id,
+                     user = input$user_id,
+                     password = input$password_id)
+      })
+
+      observeEvent(input$new_col_button, {
+        dbSendQuery(conn_fun(),sprintf(
           "ALTER TABLE %s
           ADD COLUMN %s %s ;",
           input$tbl_select,
           input$new_col_id,
           input$col_type)
-            )
-  })
+        )
+      })
 
-# ALTER TABLE table1 ADD COLUMN test_cols1 varchar(10) NOT NULL DEFAULT 'foo';
-  read_file <-
-    reactive({
-      inFile <- input$upload_tbl
-      
-      if (is.null(inFile))
-        return(NULL)
-  
-  read.csv(inFile$datapath, stringsAsFactors = FALSE)
-    })
-  
-  output$read_file_view <-
-    renderDataTable(
-      read_file()
-    )
-  # output$ls_elements <- 
-  #   renderUI({
-  #   switch(type, "numeric/double" = "numericInput()",
-  #          varchar = "textInput()") #%>% parse(text = .) 
-  #      #   %>% eval()
-  #   })
+      # ALTER TABLE table1 ADD COLUMN test_cols1 varchar(10) NOT NULL DEFAULT 'foo';
+      read_file <-
+        reactive({
+          inFile <- input$upload_tbl
 
-  
-output$tbl_select <-
-  renderUI({
-    selectizeInput("tbl_select", 
-                   "Select a table from Defined DB Conn",
-                   choices = src_tbls(dplyr_conn()))
-  })
- mod_test <- callModule(exampleMod, "my_mod",conn_fun)
- 
- # observeEvent()
- 
-  output$my_mean <- 
-    renderPrint(mod_test())
-}
+          if (is.null(inFile))
+            return(NULL)
+
+          read.csv(inFile$datapath, stringsAsFactors = FALSE)
+        })
+
+      output$read_file_view <-
+        renderDataTable(
+          read_file()
+        )
+      # output$ls_elements <-
+      #   renderUI({
+      #   switch(type, "numeric/double" = "numericInput()",
+      #          varchar = "textInput()") #%>% parse(text = .)
+      #      #   %>% eval()
+      #   })
 
 
-)
+      output$tbl_select <-
+        renderUI({
+          selectizeInput("tbl_select",
+                         "Select a table from Defined DB Conn",
+                         choices = src_tbls(dplyr_conn()))
+        })
+      mod_test <- callModule(exampleMod, "my_mod",conn_fun)
+
+      # observeEvent()
+
+      output$my_mean <-
+        renderPrint(mod_test())
+    }
+
+
+  )
 }
